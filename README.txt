e233de0f1644af2cb651f7a457db33a733c08cda
Aokijiop <Aaokijiop@gmail.com>
2023-12-27 16:42:10 -0800

    Stable commit of portfolio

---
+++ README.txt
@@ -0,0 +1,1 @@
+## Ostend Suryajaya's Personal Portfolio
---
+++ purgecss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    content: ['public_html/index.html', 'public_html/contact.html'],
+    css: ['public_html/assets/css/styles.css'],
+    output: 'dist/styles.purged.css',
+  }
